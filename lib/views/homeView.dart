c5adddb84f8607114e08b3e085feede04cec0562
salma <salmaelmaghawry91@gmail.com>
2024-04-13 16:52:42 +0200

    commit

---
+++ lib/views/homeView.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class homeView extends StatelessWidget {
+  const homeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News App'),
+      ),
+    );
+  }
+}
