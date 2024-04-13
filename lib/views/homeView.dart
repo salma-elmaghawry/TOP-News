9205170208ea8e050ed03530d29eb6ba880b2543
salma <salmaelmaghawry91@gmail.com>
2024-04-13 17:54:44 +0200

    App Bar

--- lib/views/homeView.dart
+++ lib/views/homeView.dart
@@ -7,7 +7,20 @@ class homeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('News App'),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Fresh ',
+              style: TextStyle(
+              fontSize: 23,color: Colors.amber)),
+              Text('News',
+          style: TextStyle(
+          fontSize: 23,color: Colors.black),),
+          ],
+        ),
+        centerTitle: true,
+        backgroundColor:Colors.transparent ,
+        elevation: 0,
       ),
       
     );
