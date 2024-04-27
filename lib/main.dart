8973ba9fa26950f07ed439559a10332dce91e244
salma <salmaelmaghawry91@gmail.com>
2024-04-27 17:54:05 +0300

    import Dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/views/homeView.dart';
 
 void main() {
+  getNews();
   runApp(const NewsApp());
+}
+
+final dio = Dio();
+
+void getNews() async {
+  final response = await dio.get('https://newsapi.org/v2/everything?q=sports&from=2024-03-27&sortBy=publishedAt&apiKey=cd8ebc8ba0c549788bffa0647702502f');
+  print(response);
 }
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
