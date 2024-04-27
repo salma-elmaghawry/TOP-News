dcb47094b0c6aef9b0eb6752eaceeb36c1a14e47
salma <salmaelmaghawry91@gmail.com>
2024-04-27 19:41:35 +0300

    data Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_service.dart';
 import 'package:news_app/views/homeView.dart';
 
 void main() {
+  NewsService(Dio()).getGeneralNews();
   runApp(const NewsApp());
 }
-
-
 
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -4,8 +4,10 @@ class NewsService {
   final Dio dio;
   NewsService(this.dio);
 
-  getGeneralNews()async{
+  getGeneralNews() async {
     Response response = await dio.get(
-      'https://newsapi.org/v2/top-headlines?apiKey=cd8ebc8ba0c549788bffa0647702502f&country=eg&category=general');
+        'https://newsapi.org/v2/top-headlines?apiKey=cd8ebc8ba0c549788bffa0647702502f&country=eg&category=general');
+    Map<String, dynamic> jsonData = response.data;
+    print(jsonData['articles']);
   }
 }
