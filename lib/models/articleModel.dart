6bba7de71f890b665003192140bff8e48379dd19
salma <salmaelmaghawry91@gmail.com>
2024-05-04 02:10:34 +0300

    class source

--- lib/models/articleModel.dart
+++ lib/models/articleModel.dart
@@ -1,8 +1,15 @@
 class ArticleModel {
-  final String image;
+  final String? image;
   final String title;
-  final String description;
+  final String? description;
+  final Source? source;
 
   ArticleModel(
-      {required this.image, required this.title, required this.description});
+      {required this.source,required this.image, required this.title, required this.description});
+}
+
+class Source {
+  final String id;
+  final String name;
+  Source({required this.id, required this.name});
 }
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -4,21 +4,25 @@ import 'package:news_app/models/articleModel.dart';
 class NewsService {
   final Dio dio;
   NewsService(this.dio);
-
   getGeneralNews() async {
     Response response = await dio.get(
         'https://newsapi.org/v2/top-headlines?country=us&apiKey=cd8ebc8ba0c549788bffa0647702502f');
     Map<String, dynamic> jsonData = response.data;
     //list of maps
     List<dynamic> articles = jsonData['articles'];
-
     List<ArticleModel> articlesList = [];
     //convert map to article object
+
     for (var article in articles) {
-      ArticleModel articleModel = ArticleModel(
+      ArticleModel article_model = ArticleModel(
           image: article['urlToImage'],
           title: article['title'],
-          description: article['description']);
+          description: article['description'],
+          source: Source(
+              id: article['source']['id'], name: article['source']['name']));
+
+      articlesList.add(article_model);
     }
+    print(articlesList);
   }
 }
